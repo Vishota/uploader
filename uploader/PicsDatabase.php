b3838e7bdc515cb9852a981ae390335bd3483c10
Vishota <88852731+Vishota@users.noreply.github.com>
2023-01-21 00:42:15 +0300

    removing cringe

--- uploader/PicsDatabase.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once __DIR__.'/Database/AbstractSingleDatabase.php';
-
-    class PicsDatabase extends AbstractSingleDatabase {
-        function initConnectionData() {
-            $this->setConnectionData('pics', 'root', 'mysql');
-        }
-    }
